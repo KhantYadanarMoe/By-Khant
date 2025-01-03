687a3f040039c48b4e95ebe6f4eb3becd1b26bf3
KhantYadanarMoe <khantyadanarmoe113@gmail.com>
2025-01-03 19:52:17 +0630

    By-Khant

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        logo: ["Oswald", "serif"], // Define your custom font here
+        default: ["Rubik", "serif"], // Define your custom font here
+      },
+      colors: {
+        crimson: "#DC143C", // Primary color
+        electricBlue: "#1E90FF", // Secondary color
+        limeGreen: "#32CD32", // Accent color
+        goldenYellow: "#FFD700", // Accent color
+        coral: "#FF7F50", // Accent color
+        lightGray: "#F5F5F5", // Background color
+        charcoal: "#333333", // Text color
+        softSlate: "#A9A9A9", // Border color
+      },
+    },
+  },
+  plugins: [],
+};
